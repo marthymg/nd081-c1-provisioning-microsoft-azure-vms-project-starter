8b3b8160d23a67d7806c76504081e6d7655f4970
Mueller-Glewe <muell60@gujnm275061.global.intra.guj.com>
2021-06-03 15:43:53 +0200

    cache

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -86,8 +86,7 @@ def authorized():
     if request.args.get('code'):
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = _build_msal_app(cache=cache).
-        acquire_token_by_authorization_code(
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
           request.args['code'],
           scopes=Config.SCOPE,
           redirect_uri=url_for('authorized', _external=True, _scheme='https'))
@@ -116,12 +115,15 @@ def logout():
 
 def _load_cache():
     # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
     return cache
 
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
-    pass
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
     # TODO: Return a ConfidentialClientApplication
